d72648007b56ff12173b98e8b3d1ca91a14c6da7
Clément TALVARD <c.talvard@gmail.com>
2019-04-01 17:28:00 +0200

    add rasbian ssh addon

---
+++ raspbian/run-ssh.sh
@@ -0,0 +1,15 @@
+CONFIG_PATH=/data/options.json
+
+SSH_ENABLED=$(jq --raw-output '.ssh.enabled' $CONFIG_PATH)
+SSH_LOGIN=$(jq --raw-output '.ssh.login' $CONFIG_PATH)
+SSH_PASSWORD=$(jq --raw-output '.ssh.password' $CONFIG_PATH)
+
+
+echo "==================================="
+echo "----------- CONFIG SSH ------------"
+echo "==================================="
+
+echo "SSH config: $SSH_LOGIN:$SSH_PASSWORD"
+echo "$SSH_LOGIN:$SSH_PASSWORD" | chpasswd
+
+sshd -D -ddd -e
